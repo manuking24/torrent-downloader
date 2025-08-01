3bf324f84a10d3f55987bccd03b71b66003f9196
Mahmudul Haque <mahmudul.kuet.24@gmail.com>
2025-08-01 18:52:32 +0600

    Cleaned requirements.txt and updated app

--- torrent_downloader/urls.py
+++ torrent_downloader/urls.py
@@ -7,4 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('downloader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static('/downloads/', document_root=settings.DOWNLOAD_DIR)
