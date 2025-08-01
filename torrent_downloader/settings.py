6fc29581b956b85abcddf517744482ffa7cc91ec
Mahmudul Haque <mahmudul.kuet.24@gmail.com>
2025-08-01 16:06:35 +0600

    Cleaned requirements.txt and updated app

--- torrent_downloader/settings.py
+++ torrent_downloader/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 
 # Redis Configuration
-REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+REDIS_URL = 'redis://localhost:6379/0'
 
 # Celery Configuration
 CELERY_BROKER_URL = REDIS_URL
